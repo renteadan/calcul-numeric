86af786cde911da2cad6109e7e9ae21aec6641ee
renteadan <62014192+renteadan@users.noreply.github.com>
2021-03-30 12:10:07 +0300

    pb4

--- lab3/pb2.m
+++ lab3/pb2.m
@@ -2,7 +2,6 @@
 
 for n=10:15
   c1 = cond(hilb(n));
-  ci = cond(hilb(n), Inf);
   norma = norm(hilb(n));
-  fprintf("Conditionarea de norma 1: %d\nConditionarea de norma infinita: %d\nNorma euclidiana: %d\n", c1, ci, norma);
+  fprintf("Conditionarea de norma 1: %d\nNorma euclidiana: %d\n", c1, norma);
 end
--- lab3/pb4.m
+++ lab3/pb4.m
@@ -1,10 +1,84 @@
-clc
-format long
-r = 1:20;
-% construieste radacinile polinomului cu n = 20
-c = poly(r); % polinomul cu radacinile r
-r1 = roots(c); %radacinile polinomului c
-% normrnd - genereaza un vector
-c2 = c + normrnd(0, 1e-10, 1, length(c))
-r2 = roots(c2) 
-nc = norm(r2-r')/norm(c2-c)% nr de cond
+conditionarePolinom1(14, 20);
+conditionarePolinom2(14, 20);
+
+a = 10^(-10);
+b = 0;
+y = a.*randn(20,1) + b;
+
+conditionarePolinom1(2, 20)
+perturbarePolinom1(2, 20, y)
+
+function c = conditionarePolinom1( x, n )
+    % conditionare pt polinomul de forma : (x-1)*(x-2)*...*(x-n) = 0
+    % formula generala pt conditionarea radacinii e la un polinom de grad n
+    % suma i -> 0, n-1 din |ai|*|e^i| / |e*p'(e)|
+
+    a = poly(1 : n); % an an-1 an-2 .. a1 a0
+    a = a(2 : end); % an-1 an-2 ... a0
+    a = fliplr(a); % a0 a1 ... an-1
+
+    sum = 0; % suma de forma : |a0| + |e|*|a1| + |e^2|*|a2| + .. + |e^(n-1)| * |an-1|
+    for i = 0 : n - 1 % indexarea in matlab de la 1 deci a(i+1) = ai
+       sum = sum + abs(a(i + 1)) * abs(x)^i; 
+    end
+
+    % calculam p'(e)
+    p = 1; % derivata se anuleaza cu exceptia produsului in care (x - e) e derivata
+    for i = 1:n
+        if i ~= x
+            p = p * (x - i);
+        end
+    end
+
+    c = sum / abs(x * p);
+end
+
+function c = conditionarePolinom2( e, n )
+    % conditionare pt polinomul de forma : x^n + 1/2 * x^(n-1) + .. + 1/2^n = 0
+    % formula generala pt conditionarea radacinii e la un polinom de grad n
+    % suma i -> 0, n-1 din |ai|*|e^i| / |e*p'(e)|
+
+    a = zeros(n , 1); % coeficientii
+    for i = 1:n
+        a(i) = 1 / 2^i;
+    end
+    a = fliplr(a);
+
+    sum = 0; % suma de forma : |a0| + |e|*|a1| + |e^2|*|a2| + .. + |e^(n-1)| * |an-1|
+    for i = 0 : n - 1 % indexarea in matlab de la 1 deci a(i+1) = ai
+       sum = sum + abs(a(i + 1)) * abs(e)^i; 
+    end
+
+    % calculam p'(e)
+    p = 0;
+    for i = n : -1 : 1
+        p = p + (i * e^(i - 1)) / 2^(n - i); 
+    end
+
+    c = sum / abs(e * p);
+end
+
+function c = perturbarePolinom1( e, n, perturb )
+    a = poly(1 : n); % an an-1 an-2 .. a1 a0
+    a = a(2 : end); % an-1 an-2 ... a0
+    a = fliplr(a); % a0 a1 ... an-1
+
+    for i = 1 : n
+        a(i) = a(i) + perturb(i);
+    end
+
+    sum = 0; % suma de forma : |a0| + |e|*|a1| + |e^2|*|a2| + .. + |e^(n-1)| * |an-1|
+    for i = 0 : n - 1 % indexarea in matlab de la 1 deci a(i+1) = ai
+       sum = sum + abs(a(i + 1)) * abs(e)^i; 
+    end
+
+    % calculam p'(e)
+    p = 1; % derivata se anuleaza cu exceptia produsului in care (x - e) e derivata
+    for i = 1:n
+        if i ~= e
+            p = p * (e - i);
+        end
+    end
+
+    c = sum / abs(e * p);
+end
